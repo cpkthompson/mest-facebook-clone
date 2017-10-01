3bf3694f3254c0fa7c1fef70b0f6e1cb812a0d05
myodehi <myodehi@gmail.com>
2017-10-01 13:57:29 +0000

    added users csv file where new users are saved

--- sign_up.py
+++ sign_up.py
@@ -1,28 +1,55 @@
-import time
+import os.path
 import datetime
+import csv
+
+
+class Users:
+
+    def __init__(self, users = []):
+        self.users = users
+
+    def users_list(self, user):
+        self.users.append(user)
+
+    def create_csv(self, user):
+
+        file_exist = os.path.isfile('user.csv')
+        with open('user.csv', 'a') as csv_file:
+            fieldnames = ['firstname', 'lastname', 'email', 'password', 'birthday', 'gender']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+            if not file_exist:
+                writer.writeheader()
+            writer.writerow(
+                {'firstname': user.firstname, 'lastname': user.lastname, 'email': user.email, 'password': user.password, 'birthday': user.birthday, 'gender': user.gender})
+
 
 class SignUp:
 
+    def __init__(self, firstname ='', lastname ='', email ='', password ='', birthday ='', gender =''):
 
-    def __init__(self, firstname, lastname, email, password, birthday, gender):
+        self.firstname = input("Enter your firstname: ")
+        self.lastname = input("Enter your lastname: ")
+        self.email = input("Enter your email: ")
+        self.password = input("Enter your password: ")
+        self.birthday = input("Enter your birthday: ")
+        self.gender = input("Enter your gender: ")
 
-        self.firstname = firstname
-        self.lastname = lastname
-        self.email = email
-        self.password = password
-        self.birthday = birthday
-        self.gender = gender
+def main():
 
-    def choose_birthday(self):
+    while True:
+        new_user = Users()
+        register = int(input('Type 1 to signup as a new user: '))
 
-     date = int(self.birthday)
-     self.birthday = datetime.date(date)
+        if register == 1:
+            user = SignUp()
+            new_user.users_list(user)
+            new_user.create_csv(user)
 
+        elif register == 2:
+            print("incorrect input")
+        else:
+            print("End here")
+            break
 
-     def __repr__(self):
-        return self.firstname or self.lastname
-
-user1 = SignUp("Anty", "Tope", "tope@gmail.com", "aasmdmdmj", "25jan", "female")
-print(user1)
-user1 = SignUp("Anty", "Tope", "tope@gmail.com", "aasmdmdmj", "25jan", "female")
-print(user1)
+main()
