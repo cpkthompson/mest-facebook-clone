31b59f7119d0e923d646a2e71cc2b98d5692d94d
Aubrey Malebo Ndiza <aubrey.ndiza@meltwater.org>
2017-10-02 18:58:40 +0000

    like page code started

---
+++ fbpage_likepage.py
@@ -0,0 +1,16 @@
+import fb_page.py
+import csv
+
+
+class Likes:
+
+    def __init__(self, current_likes, new_likes, all_likes):
+        self.current_likes = current_likes
+        self.new_likes = new_likes
+        self.all_likes = all_likes
+
+    def like_page(self):
+
+    de
+
+
