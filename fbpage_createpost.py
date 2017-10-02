7ad1d1947dac62df379c46962869255bfd076ea1
Aubrey Malebo Ndiza <aubrey.ndiza@meltwater.org>
2017-10-02 18:57:45 +0000

    create post code started

---
+++ fbpage_createpost.py
@@ -0,0 +1,66 @@
+import sign_up.py
+import user.csv
+
+class User:
+    
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+    email = input("Enter Your Email")
+    password = input("Enter Your Password")
+    print("Verification In Progress")
+
+        def verify_user(self, user):
+            
+            if self.email == SignUp(user.email)
+                print("Welcome Page Admin")
+
+            else:
+                self.email != user.email
+                print("Access Denied!!!")
+    
+    def create_post():
+        pass
+    
+    def edit_post():
+        pass
+    
+    def delete_post():
+        pass
+
+Admin = User()
+
+class Page(User):
+    
+    def __init__(self):
+        pass
+
+    def post_details(self, content):
+        self.content = content
+        
+    
+    def delete_post(self):
+        pass
+    
+    def create_post():
+        
+
+class Page_Details:
+    
+    def __init__(self, content):
+        self.content = content
+
+class Users:
+
+    def __init__(self):
+        pass
+
+    def like_or_dislike_page(self):
+        pass
+    
+    def follow_page(self):
+        pass
+    
+    def share_page(self):
+        pass
