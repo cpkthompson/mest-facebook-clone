467e29f09d419773e9a090c2a90520d39b3f97b6
myodehi <myodehi@gmail.com>
2017-10-02 19:24:09 +0000

    created a new sign in and sign

--- sign_in.py
+++ sign_in.py
@@ -1,4 +1,7 @@
-import sign_up.py
+import sign_up
+import user
+import csv
+
 
 class SignIn:
 
@@ -7,7 +10,23 @@ class SignIn:
         self.email = email
         self.password = password
 
+    def verify_sign_in(self):
 
+        #reading the user details in the csv and spliting it to make a list to get index of email
+        with open('user.csv', 'r') as csv_file:
+            for user in csv_file:
+                stored_email = user.split(",")[2]
+                # user.split[2] get the index of email in the csv file
+
+                if stored_email == self.email:
+                    return "welcome", self.firstname
+                else:
+                    return "wrong email"
+
+
+if __name__ == "__main__":
+    old = SignIn("abc", "wwwww")
+    old.verify_sign_in()
 
 
 
--- sign_up.py
+++ sign_up.py
@@ -1,75 +1,33 @@
-import os.path
-import datetime
-import csv
+import user
+import sign_in
+#This is for sign up and sign in
+
+while True:
+    register = int(input('Type 1 to sign up as a new user, 2 to sign in: '))
+
+    if register == 1:
+        firstname = input("Enter your lastname: ")
+        lastname = input("Enter your lastname: ")
+        email = input("Enter your email: ")
+        password = input("Enter your password: ")
+        birthday = input("Enter your birthday: ")
+        gender = input("Enter your gender: ")
+
+        new_user = user.User(firstname, lastname, email, password, birthday, gender)
+        view = new_user.verify_email()
+        print(view)
+        new_user.save_to_csv()
+
+    elif register == 2:
+        email = input("Enter your email: ")
+        password = input("Enter your password: ")
+
+        old_user = sign_in.SignIn(email, password)
+        view = old_user.verify_sign_in()
+        print(view)
 
 
-class Users:
 
-    def __init__(self, users = []):
-        self.users = users
-
-    def users_list(self, user):
-        self.users.append(user)
-
-    def create_csv(self, user):
-
-        file_exist = os.path.isfile('user.csv')
-        with open('user.csv', 'a') as csv_file:
-            fieldnames = ['firstname', 'lastname', 'email', 'password', 'birthday', 'gender']
-            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
-            if not file_exist:
-                writer.writeheader()
-            writer.writerow(
-                {'firstname': user.firstname, 'lastname': user.lastname, 'email': user.email, 'password': user.password, 'birthday': user.birthday, 'gender': user.gender})
-
-
-class SignUp:
-
-    def __init__(self, firstname ='', lastname ='', email ='', password ='', birthday ='', gender =''):
-
-        self.firstname = input("Enter your firstname: ")
-        self.lastname = input("Enter your lastname: ")
-        self.email = input("Enter your email: ")
-        self.password = input("Enter your password: ")
-        self.birthday = input("Enter your birthday: ")
-        self.gender = input("Enter your gender: ")
-
-
-class SignIn:
-
-    def __init__(self, email ='', password =''):
-
-        self.email = input("Enter your email: ")
-        self.password = input("Enter your password: ")
-
-    def verify_sign(self, user):
-
-        if self.email == SignUp(user.email):
-            print("Successful")
-
-        else:
-            self.email != user.email
-            print("invalid email")
-
-
-def main():
-
-    while True:
-        new_user = Users()
-        register = int(input('Type 1 to sign up as a new user, 2 to sign in: '))
-
-        if register == 1:
-            user = SignUp()
-            new_user.users_list(user)
-            new_user.create_csv(user)
-
-        elif register == 2:
-            user = SignIn()
-            new_user.verify_sign(user)
-
-        else:
-            print("End here")
-            break
-
-main()
+    else:
+        print("End here")
+        break
